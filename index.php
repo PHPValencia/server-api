78c01a0859c44451e0d9596dbf1c27704bc9eeef
Stephen O'Flynn <flynniec6@gmail.com>
2016-03-22 21:23:45 +0100

    Add a base index page

---
+++ index.php
@@ -0,0 +1,12 @@
+<?php
+require_once __DIR__.'/vendor/autoload.php'; 
+
+date_default_timezone_set('Europe/Madrid');
+
+$app = new Silex\Application(); 
+
+$app->get('/', function($name) use($app) { 
+    return 'PHP Valencia API';
+}); 
+
+$app->run(); 
