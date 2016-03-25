b27c7d254ae385434d714a0f48d319e3d6b2f03e
Stephen O'Flynn <flynniec6@gmail.com>
2016-03-25 23:34:46 +0100

    Move views folder

--- src/app.php
+++ src/app.php
@@ -1,7 +1,7 @@
 <?php
 
 $app->register(new Silex\Provider\TwigServiceProvider(), array(
-    'twig.path' => __DIR__.'/views',
+    'twig.path' => __DIR__.'/../views',
 ));
 
 $app->get('/', function() use($app) { 
